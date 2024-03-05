eae6c15eb5fb73bb1d8e805a64b71b53be1348bc
isai34232 <leicaj4@gmail.com>
2024-03-04 19:09:10 -0600

    actualizacion

--- Sistemas_Ecuaciones.py
+++ Sistemas_Ecuaciones.py
@@ -1,35 +1,38 @@
 import random
 
-# Definimos la función para evaluar el sistema de ecuaciones
+# Definir la función para evaluar el sistema de ecuaciones
 def evaluar_ecuaciones(B, D, E, F):
+    # Definición de las ecuaciones del sistema
     ecuacion_1 = 16*B - 6*D + 4*D + F
     ecuacion_2 = B + 8*D + E + F
     ecuacion_3 = 16*B + 2*D - 4*E + F
     ecuacion_4 = 9*B + 8*D - 3*E + F
-
-    return (ecuacion_1 ) + (ecuacion_2 ) + (ecuacion_3 ) + (ecuacion_4 )
+    # Devolver la suma de las ecuaciones como resultado
+    return (ecuacion_1) + (ecuacion_2) + (ecuacion_3) + (ecuacion_4)
 
 # Definir parámetros de búsqueda
 limite_inferior = -100
 limite_superior = 100
 num_intentos = 10
-#el valor de none se utiliza para almacenar la mejor solucion posible
+# Inicializar la mejor solución y su valor
 mejor_solucion = None
 mejor_valor = float('inf')
 
 # Realizar búsqueda aleatoria
 for _ in range(num_intentos):
+    # Generar valores aleatorios para B, D, E, y F dentro de los límites especificados
     B = random.randint(limite_inferior, limite_superior)
     D = random.randint(limite_inferior, limite_superior)
     E = random.randint(limite_inferior, limite_superior)
     F = random.randint(limite_inferior, limite_superior)
     
+    # Evaluar el sistema de ecuaciones con los valores aleatorios generados
     valor_actual = evaluar_ecuaciones(B, D, E, F)
     
+    # Actualizar la mejor solución si se encuentra un valor menor
     if valor_actual < mejor_valor:
         mejor_valor = valor_actual
         mejor_solucion = (B, D, E, F)
-        
 
 # Mostrar la mejor solución encontrada
 print("La mejor solución encontrada es:")
